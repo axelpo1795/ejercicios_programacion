4c22298eecaccdf5789d8351fe18ab9869965601
axelpo17 <axel1795@gmail.com>
2025-12-19 18:38:52 -0600

    Revert "Anadir ejercicios"
    This reverts commit b90576e058b5061cc3c57952db8ff704e322b8f7.

--- S4/Ejercicio4_S4.py
+++
@@ -1,7 +0,0 @@
-#Ejercicio4_S4.py
-num1 = float(input("Digite el primer número: "))
-num2 = float(input("Digite el segundo número: "))
-num3 = float(input("Digite el tercer número: "))
-
-largest = max(num1, num2, num3)
-print(f"El número mayor es {largest}")
--- S5/Ejercicio3_S5.py
+++
@@ -1,9 +0,0 @@
-my_list = [4, 3, 6, 1, 7]
-
-first = my_list[0]
-last = my_list[-1]
-
-my_list[0] = last
-my_list[-1] = first
-
-print(my_list)
